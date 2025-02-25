1bfa7950d04cf2b67747b587d61af17b1684777f
Anonymous4822 <kenjisupremacy@MacBook-Pro-de-Kenji.local>
2025-02-25 11:53:05 -0400

    docker files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+# Lire la variable d'environnement MESSAGE
+message = os.getenv("MESSAGE", "Bienvenue dans votre application web minimaliste !")
+
+@app.route("/")
+def home():
+    return f"<h1>{message}</h1>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
+
